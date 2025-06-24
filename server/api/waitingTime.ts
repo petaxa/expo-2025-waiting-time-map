d9449378969362ed9edc62aca51ba1190658c5ae
tempestif <soccer.i_y@icloud.com>
2025-06-25 03:35:50 +0900

    json の公開先変更に対応

--- server/api/waitingTime.ts
+++ server/api/waitingTime.ts
@@ -7,8 +7,7 @@ export default defineEventHandler(
       postedAt: string;
     }[]
   > => {
-    const url =
-      "https://petaxa.github.io/expo-2025-waiting-time-json/waiting-time.json";
+    const url = "https://content.expo-waiting-map.petaxa.com/waiting-time.json";
     const res = await fetch(url);
     const json = await res.json();
     return json.data;
